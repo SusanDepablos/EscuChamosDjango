cbc141146a7ab3e3b43a1e5df328946dc02f5417
SusanDepablos <susandepablos14@gmail.com>
2024-08-16 18:32:50 -0400

    Index

--- EscuChamosDjango/urls.py
+++ EscuChamosDjango/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from core.views import home  # Asegúrate de importar tu vista aquí
+from core.views import home, index  # Asegúrate de importar tu vista aquí
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.api_urls.urls')),
     path('', home),  # Ruta para la vista en la raíz
+    path('index', index),  # Ruta para la vista en la raíz
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def index(request):
+    return render(request, 'index.html')
