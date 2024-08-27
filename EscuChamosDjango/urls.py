eede3e11fc482a0a4cc6bbe61ba64e0f570f605d
Valentinaparra1234 <valentinasharait@gmail.com>
2024-08-27 13:14:55 -0400

    pagina 404

--- EscuChamosDjango/urls.py
+++ EscuChamosDjango/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from core.views import index  # Asegúrate de importar tu vista aquí
+from core.views import index, error  # Asegúrate de importar tu vista aquí
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.api_urls.urls')),
     path('', index),  # Ruta para la vista en la raíz
+    path('error/', error),  # Ruta para la vista en la raíz
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
