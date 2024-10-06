ebb451d0a74a061fe55dc4d4af8d2028e61737a9
SusanDepablos <susandepablos14@gmail.com>
2024-10-06 12:34:16 -0400

    coma

--- core/management/commands/seeders.py
+++ core/management/commands/seeders.py
@@ -79,7 +79,7 @@ class Command(BaseCommand):
         type_posts = [
             ('Normal', 'Publicación normal de texto'),
             ('Multimedia', 'Publicación que contiene imágenes, videos o audios'),
-            ('Republicado', 'Publicación Republicada')
+            ('Republicado', 'Publicación Republicada'),
             ('Escuchamos', 'Publicación donde los voluntarios y administradores pueden interactuar con la comunidad')
         ]
         for name, description in type_posts:
