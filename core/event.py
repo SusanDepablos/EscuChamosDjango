cb7318db1755f32f16c61f1f00a9bb7f02ff93c0
SusanDepablos <susandepablos14@gmail.com>
2024-10-18 22:11:12 -0400

    diosito tu me quieres y yo te quiero

--- core/event.py
+++ core/event.py
@@ -1,37 +1,33 @@
 from django.http import StreamingHttpResponse
-import time
+import asyncio
 from .models import Notification
 
 clients = {}
 
 class LiveNotification:
     @staticmethod
-    def stream(req, user_id):
+    async def stream(req, user_id):
         if user_id not in clients:
             clients[user_id] = []
 
         clients[user_id].append(req)
 
-        def generate():
-            count = LiveNotification.get_count(user_id)
-            yield f"data: {{ 'notifications': {count} }}\n\n"
+        count = await LiveNotification.get_count(user_id)
+        yield f"data: {{ 'notifications': {count} }}\n\n"
 
-            try:
-                while True:
-                    new_count = LiveNotification.get_count(user_id)
-                    if new_count != count:
-                        count = new_count
-                        yield f"data: {{ 'notifications': {count} }}\n\n"
-
-                    time.sleep(1)
-            finally:
-                clients[user_id].remove(req)
-
-        return generate()
+        try:
+            while True:
+                new_count = await LiveNotification.get_count(user_id)
+                if new_count != count:
+                    count = new_count
+                    yield f"data: {{ 'notifications': {count} }}\n\n"
+                await asyncio.sleep(1)  # Usar asyncio.sleep para evitar bloqueo
+        finally:
+            clients[user_id].remove(req)
 
     @staticmethod
-    def get_count(user_id):
-        return Notification.objects.filter(receiver_user_id=user_id, is_read=False).count()
+    async def get_count(user_id):
+        return await Notification.objects.filter(receiver_user_id=user_id, is_read=False).acount()
 
     @staticmethod
     def sse_endpoint(req, user_id):
